a29aab755a5d0e74906270356ffefb697691b2b2
Atanas <atanasdraganov77@gmail.com>
2022-12-03 23:52:40 +0200

    changing title after 3 secs

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  setTimeout(function(){
+    document.title = "One new message"
+  }, 3000);
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
+    
     alert("💣");
   });
 });
